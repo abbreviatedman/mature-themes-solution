593b00f83032bf5eaf83181e1848b74a1f90720d
Colin Jaffe <balloonasaurus@gmail.com>
2019-04-15 14:38:26 -0400

    finishes basic codealong version

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 window.onload = init;
 
 function init() {
-    const toggleButtons = Array.from(document.querySelectorAll('.toggle-button'));
-    toggleButtons.forEach((button) => button.addEventListener('click', toggleTheme));
+    document.querySelector('.toggle-button')
+        .addEventListener('click', toggleTheme);
 }
 
 function toggleTheme(event) {
     event.preventDefault();
 
-    const toggleButtons = Array.from(document.querySelectorAll('.toggle-button'));
-    toggleButtons.forEach((button) => button.classList.toggle('dark-theme'));
+    const app = document.querySelector('.app');
+    const button = event.target;
 
+    app.classList.toggle('dark-theme');
+    button.classList.toggle('dark-theme');
 
-    // One by one approach, if you think the Array.from and forEach is overkill
-    // for two elements.
-    const toggleButton1 = document.querySelector('.toggle-button-1')
-    const toggleButton2 = document.querySelector('.toggle-button-2')
-    
-    toggleButton1.classList.toggle('.dark-theme')
-    toggleButton2.classList.toggle('.dark-theme')
+    button.innerText = button.innerText === 'SWITCH TO DARK MODE'
+    ? 'SWITCH TO LIGHT MODE'
+    : 'SWITCH TO DARK MODE';
 }
-
-const upArray = (arr) => (parseFloat(arr.map((num) => num.toString()).join('')) + 1).toString().split('').map((num) => parseFloat(num));
