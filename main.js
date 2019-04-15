f0dbfcd30539470dee05d249d8988b1672c83592
Colin Jaffe <colin.jaffe@gmail.com>
2019-04-15 11:08:53 -0400

    moving to other machine

--- main.js
+++ main.js
@@ -10,4 +10,15 @@ function toggleTheme(event) {
 
     const toggleButtons = Array.from(document.querySelectorAll('.toggle-button'));
     toggleButtons.forEach((button) => button.classList.toggle('dark-theme'));
+
+
+    // One by one approach, if you think the Array.from and forEach is overkill
+    // for two elements.
+    const toggleButton1 = document.querySelector('.toggle-button-1')
+    const toggleButton2 = document.querySelector('.toggle-button-2')
+    
+    toggleButton1.classList.toggle('.dark-theme')
+    toggleButton2.classList.toggle('.dark-theme')
 }
+
+const upArray = (arr) => (parseFloat(arr.map((num) => num.toString()).join('')) + 1).toString().split('').map((num) => parseFloat(num));
